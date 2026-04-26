4acb90d6509357dd08d2ca05e13a4eeda8f7b1b2
Felps <felipebelomi@gmail.com>
2026-04-26 14:20:49 -0300

    jogo entregue para avaliação

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) {
         Dragao drako = new Dragao("Drako", 100, 50, 50, 35);
         Lobo lobinho = new Lobo("Lobinho", 45, 30, 25, 10);
-        Golem golem = new Golem("Golem", 70, 35, 50, 80);
+        Golem golem = new Golem("Golem", 50, 35, 50, 80);
 
         Criatura criaturas[] = new Criatura[3];
 
@@ -34,5 +34,19 @@ public class App {
             criaturas[i].mostrarStatus();
         }
         System.out.println("--------- FIM STATUS FINAL ---------\n");
+
+        System.out.println("--------- CRIATURA VENCEDORA ---------");
+        int posicaoVencedor = 0;
+        for (int i = 0; i < criaturas.length; i++) {
+            for (int x = 1; x < criaturas.length; x++) {
+                if (criaturas[i].getVida() > criaturas[j].getVida()) {
+                    posicaoVencedor = i;
+                }
+            }
+        }
+
+        System.out.println("Criatura com mais vida: " + criaturas[posicaoVencedor].getNome());
+        System.out.println("--------- FIM CRIATURA VENCEDORA ---------\n");
+
     }
 }
